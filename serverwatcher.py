bae73f9994ad828bfcb534e866dde856c92f2a67
wak <>
2022-09-24 17:44:50 +0900

    fix script

--- serverwatcher.py
+++ serverwatcher.py
@@ -10,7 +10,7 @@ PTN_LEFT = 'left the game'
 # setup log
 logger = logging.getLogger('mylog')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(os.getcwd() + '/mylog.log')
+handler = logging.FileHandler(os.getcwd() + '/log.log')
 logger.addHandler(handler)
 fmt = logging.Formatter('%(asctime)s %(message)s')
 handler.setFormatter(fmt)
@@ -23,13 +23,15 @@ def stop_server():
 
 def is_disabled_time():
     # 起動後15分間はシャットダウン対象外とする
-    # systemctlコマンドの結果パターン：'水 2022-09-21 15:26:25 UTC'の曜日を削り、経過時間を取得
+    # systemctlコマンドの結果パターン：'水 2022-09-21 15:26:25 UTC\n'の曜日を削り、経過時間を取得
     res = subprocess.run('systemctl show --property=ActiveEnterTimestamp minecraft.service | cut -d= -f2',
                          shell=True, capture_output=True, text=True)
-    started = dt.strptime(res.stdout[2:-1], '%Y-%m-%d %H:%M:%S %Z')
+    time = ' '.join(res.stdout.replace('\n', '').split(' ')[1:])
+    started = dt.strptime(time, '%Y-%m-%d %H:%M:%S %Z')
     time_diff = dt.now() - started
     logger.debug('uptime: ' + str(time_diff.seconds))
     if time_diff.seconds < 15 * 60:
+        logger.debug('Script exit because it is within 15 minutes after startup.')
         return True
     return False
 
